aa782065840993e6600cfe1de60757e031ab3f19
Sharad Satsangi <s.satsangi@gmail.com>
2020-09-08 14:26:29 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,3 +2,6 @@
 first_number = 0.1
 second_number = 0.33333
 sum = second_number + first_number
+difference = first_number - second_number
+product = second_number * first_number
+quotient = second_number / first_number
