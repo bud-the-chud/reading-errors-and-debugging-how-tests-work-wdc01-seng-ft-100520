bee4c496a70906df0514c29dd23d1faade2c41f2
Sharad Satsangi <s.satsangi@gmail.com>
2020-09-08 14:26:53 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,4 +4,4 @@ second_number = 0.33333
 sum = second_number + first_number
 difference = first_number - second_number
 product = second_number * first_number
-quotient = second_number / first_number
+quotient = first_number / second_number
